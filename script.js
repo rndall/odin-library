0b33b1c8c0dd8fb6479fb00956393be226c90081
rndall <96964503+rndall@users.noreply.github.com>
2025-01-12 23:48:01 +0800

    Write function to display books

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
+const yourBooks = document.querySelector("#your-books");
+
 const myLibrary = [];
+
+addBookToLibrary("Test1", "Elon Musk", 23, false);
+addBookToLibrary("Test2", "Hana", 89, true);
+
+displayBooks();
 
 function Book(title, author, pages, read) {
 	this.title = title;
@@ -12,4 +19,30 @@ function Book(title, author, pages, read) {
 function addBookToLibrary(title, author, pages, read) {
 	const book = new Book(title, author, pages, read);
 	myLibrary.push(book);
+}
+
+function displayBooks() {
+	for (const book of myLibrary) {
+		const card = document.createElement("div");
+		card.classList.add("card");
+
+		const title = document.createElement("h3");
+		title.classList.add("card__title");
+		title.textContent = book.title;
+
+		const author = document.createElement("p");
+		author.classList.add("card__text");
+		author.textContent = book.author;
+
+		const pages = document.createElement("p");
+		pages.classList.add("card__text");
+		pages.textContent = book.pages;
+
+		const read = document.createElement("p");
+		read.classList.add("card__text");
+		read.textContent = book.read ? "Already Read" : "Not Read Yet";
+
+		card.append(title, author, pages, read);
+		yourBooks.appendChild(card);
+	}
 }
