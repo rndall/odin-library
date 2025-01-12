9f864a746636a509449da428e75fc787dd438d10
rndall <96964503+rndall@users.noreply.github.com>
2025-01-12 17:46:26 +0800

    Write constructor for making book objects

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 const myLibrary = [];
+
+function Book(title, author, pages, read) {
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.read = read;
+	this.info = () =>
+		`${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "already read" : "not read yet"}`;
+}
